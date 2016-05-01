2f3079c9bf2c67ed5bb9e186979418f363554dd9
Victor Tang <victortang1992@gmail.com>
2016-05-01 16:55:15 -0700

    client js revamp

--- public/app.js
+++ public/app.js
@@ -1,9 +1,59 @@
- document.addEventListener("DOMContentLoaded", function() {
+var _ = {
 
-	var processDropped = function(eventTarget){
+	getInteractables : function(){
+		_.page = document.querySelector("html");
+		_.uploadbtn = document.getElementById('upload-btn');
+		_.fileupload = document.getElementById('fileUpload');
+		_.dropzone = document.getElementById('drop-zone');
+	},
+
+	initEvents : function(){
+		_.uploadbtn.addEventListener("click", function () {
+			_.uploadedFileCheck();
+		}),
+
+		////////////////  drag and drop
+		// prevent missed drops == load file
+		_.page.addEventListener("dragover", function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			e.dataTransfer.dropEffect = "none";
+		}, false);
+		_.page.addEventListener("drop", function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+		}, false);
+
+		//dropzone1 upload
+		_.dropzone.addEventListener('dragleave', function (e) {
+			if (_.dropzone.classList.contains("onTop")) {
+				_.dropzone.classList.remove("onTop")
+			}
+		});
+
+		_.dropzone.addEventListener("dragover", function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			if (!(_.dropzone.classList.contains("onTop"))) {
+				_.dropzone.classList.add("onTop")
+			}
+			e.dataTransfer.dropEffect = "copy";//mouse icon
+		}, false);
+
+		_.dropzone.addEventListener("drop", function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			if (_.dropzone.classList.contains("onTop")) {
+				_.dropzone.classList.remove("onTop")
+			}
+			_.processDropped(e, 1)
+		}, false);
+	},
+
+	processDropped : function(eventTarget){
         // let's just work with one file
         var file = eventTarget.dataTransfer.files[0];
-        var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.txt)$/;
+        var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.wav|.mp3|.ogg|.flac|.wma)$/;
         //console.log("Type: " + file.type);                        for debug use
         //console.log(regex.test(file.name.toLowerCase()));
         if (regex.test(file.name.toLowerCase())) {
@@ -18,10 +68,10 @@
         } else {
         alert("Please upload a valid CSV file.");
         }
-	}
-	var uploadedFileCheck = function(str){
-		var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.tsv|.csv|.txt)$/;
-        if (regex.test($(str).val().toLowerCase())) {
+	},
+	uploadedFileCheck : function(){
+		var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.wav|.mp3|.ogg|.flac|.wma)$/;
+        if (regex.test(_.fileupload.value.toLowerCase())) {
             if (typeof (FileReader) != "undefined") {
 				
 				//PREPARE DATA AND SEND
@@ -41,48 +91,9 @@
         }
 	}
 
-    $("#upload-btn").on("click", function () {
-		uploadedFileCheck("#fileUpload",1);
-    });
+}
 
-    ////////////////  drag and drop
-	// prevent missed drops == load file
-    var page = document.querySelector("html");
-	page.addEventListener("dragover", function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        e.dataTransfer.dropEffect = "none";
-    }, false);
-    page.addEventListener("drop", function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-    }, false);
-
-	//dropzone1 upload
-    var dropzone = document.querySelector("#drop-zone");
-
-    dropzone.addEventListener('dragleave', function (e) {
-        if ($("#drop-zone").hasClass("onTop")) {
-            $("#drop-zone").removeClass("onTop")
-        }
-    });
-
-    dropzone.addEventListener("dragover", function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        if (!$("#drop-zone").hasClass("onTop")) {
-            $("#drop-zone").addClass("onTop")
-        }
-        e.dataTransfer.dropEffect = "copy";//mouse icon
-    }, false);
-
-    dropzone.addEventListener("drop", function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        if ($("#drop-zone").hasClass("onTop")) {
-            $("#drop-zone").removeClass("onTop")
-        }
-		processDropped(e, 1)
-    }, false);
-
-});
+ document.addEventListener("DOMContentLoaded", function() {
+	 _.getInteractables();
+	 _.initEvents();
+ });
