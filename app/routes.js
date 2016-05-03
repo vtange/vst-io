d276b30563a6610f586ebf0eacc33e8b265fd36f
Victor Tang <victortang1992@gmail.com>
2016-05-03 15:59:52 -0700

    scaffold promises

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,12 @@
 var multer  = require('multer');
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage, dest: './tmp/'});
+var Promise = require("bluebird");
 
 //VST host
 var VSTHost = require("node-vst-host").host;
- 
 var host = new VSTHost();
- 
+
 // Print our a list of the available plugins 
 host.listPlugins( function(names) {
 	console.log( "Available Plugins");
@@ -34,17 +34,28 @@ var addStandardListeners = function(command) {
 	    console.log('Sox command succeeded!');
 	});
 };
-var toWav = function(fileName, outputName) {
-	var command = SoxCommand();
-	command.input(fileName);
-	command.output(outputName)
-		.outputFileType('wav')
+function toWav(fileName, outputName) {
+	return new Promise(function(resolve, reject) {
+		var command = SoxCommand();
+		command.input(fileName);
+		command.output(outputName)
+			.outputFileType('wav')
 
-	addStandardListeners(command);
-	command.run()
-	return command;
+		addStandardListeners(command);
+		command.run(reject, resolve);
+	});
 };
-
+function deEss(fileName, outputName) {
+	return new Promise(function(resolve, reject) {
+		host.processAudio( __dirname + "/" + "utterance_0.wav", __dirname + "/" + "utterance.wav", [{name:__dirname +"/"+"test_VST.dll", preset:__dirname +"/"+"test_VST.fxp"}]);
+			/*^.err(function(){
+				//woops, couldn't process your file
+			}).then(function(){
+				//move on to vst application
+				
+			})*/
+	});
+};
 
 var uploadFile = function(req, res) {
 	console.log(req.files);
@@ -55,7 +66,16 @@ var uploadFile = function(req, res) {
 			res.status(500).send(err);
 		}
 		else {
+			//determine what process from req.body
+			
+			//default processing
 			toWav(__dirname + '/' +  req.files.userfile.name, req.files.userfile.name.slice(0,req.files.userfile.name.length-4)+'.wav');
+			/*^.err(function(){
+				//woops, couldn't process your file
+			}).then(function(){
+				//move on to vst application
+				
+			})*/
 		}
 	});
 };
