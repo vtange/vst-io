31eb778231775452b997197b8afb2c51b9d7535a
Victor Tang <victortang1992@gmail.com>
2016-05-19 15:08:33 -0700

    redesign p2

--- public/app.js
+++ public/app.js
@@ -9,6 +9,8 @@ var _ = {
 	},
 
 	initEvents : function(){
+
+		////////////////  sends selected file
 		_.uploadbtn.addEventListener("click", function () {
 			_.uploadedFileCheck();
 		}),
@@ -49,6 +51,32 @@ var _ = {
 			}
 			_.processDropped(e, 1)
 		}, false);
+		
+		////////////////  assign custom inputs
+		var inputs = document.querySelectorAll( '.inputfile' );
+		Array.prototype.forEach.call( inputs, function( input )
+		{
+			var label	 = input.nextElementSibling,
+				labelVal = label.innerHTML;
+
+			input.addEventListener( 'change', function( e )
+			{
+				var fileName = '';
+				if( this.files && this.files.length > 1 )
+					fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+				else
+					fileName = e.target.value.split( '\\' ).pop();
+
+				if( fileName )
+					label.querySelector( 'span' ).innerHTML = fileName;
+				else
+					label.innerHTML = labelVal;
+			});
+
+			// Firefox bug fix
+			input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
+			input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
+		});
 	},
 
 	processDropped : function(eventTarget){
