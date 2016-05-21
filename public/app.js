eb3e79cc55516fb322d5d12d0d3fe49f28b7909b
Victor Tang <victortang1992@gmail.com>
2016-05-21 00:26:37 -0700

    basic app cycle workflow

--- public/app.js
+++ public/app.js
@@ -1,21 +1,54 @@
 var _ = {
+	//used to check file extension
 	validFiles : /^([a-zA-Z0-9\s_\\.\-:\(\)])+(.wav|.mp3|.ogg|.flac|.wma)$/,
+	
+	//used to check where user is on workflow
+	state : { workflow : 1 },
+	
+	//[init at DOM load] gets HTML elements for targeting
 	getInteractables : function(){
 		_.page = document.querySelector("html");
+		_.workflow = document.getElementById("workflow");
 		_.uploadbtn = document.getElementById('upload-btn');
 		_.fileupload = document.getElementById('file-upload');
 		_.filelabel = document.getElementById('file-upload').nextElementSibling;
 		_.filename = _.filelabel.innerHTML;
 		_.dropzone = document.getElementById('drop-zone');
 		_.progressbar = document.getElementById('progress-bar');
+		_.next = document.getElementById('next-btn');
+		_.back = document.getElementById('back-btn');
 	},
 
+	//[init after getInteractables] assigns events
 	initEvents : function(){
+		////////////////  OBJECT-WATCH state
+		_.state.watch("workflow", function () {
+			if(this.workflow===1){
+				_.workflow.style.webkitTransform = 'translateX(-'+0+'%)';
+				_.workflow.style.mozTransform    = 'translateX(-'+0+'%)';
+				_.workflow.style.transform       = 'translateX(-'+0+'%)';
+			}
+			else{
+				_.workflow.style.webkitTransform = 'translateX(-'+100+'%)';
+				_.workflow.style.mozTransform    = 'translateX(-'+100+'%)';
+				_.workflow.style.transform       = 'translateX(-'+100+'%)';
+			}
+		});
+
+		////////////////  next cycles forward
+		_.next.addEventListener("click", function () {
+			_.cycleWorkflow();
+		});
+
+		////////////////  back cycles backward
+		_.back.addEventListener("click", function () {
+			_.cycleWorkflow();
+		});
 
 		////////////////  sends selected file
 		_.uploadbtn.addEventListener("click", function () {
 			_.sendFiles();
-		}),
+		});
 
 		////////////////  drag and drop
 		// prevent missed drops == load file
@@ -51,10 +84,9 @@ var _ = {
 			}
 			_.processDropped(e, 1)
 		}, false);
-		
+
 		//////////////// default file add
-		_.fileupload.addEventListener( 'change', function( e )
-		{
+		_.fileupload.addEventListener( 'change', function(e) {
 			var fileName = '';/* prevent multi file upload for now
 			if( this.files && this.files.length > 1 )
 				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
@@ -106,7 +138,6 @@ var _ = {
 				  showInfo('An error occurred while submitting the form. Maybe your file is too big');
 				};
 				request.onload = function() {
-				  showInfo(this.statusText);
 				};
 
 				//send request
@@ -118,7 +149,11 @@ var _ = {
             alert("One or more of your files is not a valid audio file.");
         }
 	},
+	cycleWorkflow: function() {
+		_.state.workflow = _.state.workflow === 1 ? 2 : 1;
+	},
 	showInfo: function(message) {
+		//leftover Jquery
 		$('div.progress').hide();
 		$('strong.message').text(message);
 		$('div.alert').show();
